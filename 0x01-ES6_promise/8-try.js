8fffa73ffc0ad78b4f53761c7f5d6918cfd3670b
Sma3in96 <oumouss.ismail@gmail.com>
2024-05-29 01:53:08 +0100

    setup

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,10 @@
 export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
   try {
     return numerator / denominator;
   } catch (err) {
-    throw new Error('cannot divide by 0');
+    return err;
   }
 }
