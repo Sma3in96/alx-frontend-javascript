87fc1e8893a8ad45c9e79bec54a32f9b30a78cac
Sma3in96 <oumouss.ismail@gmail.com>
2024-05-31 01:20:35 +0100

    classroom

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -21,4 +21,45 @@ export default class HolbertonCourse {
       throw new Error('Students must be a array');
     }
   }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new Error('Name must be a string');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new Error('Length must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (Array.isArray(students)) {
+      for (const student of students) {
+        if (typeof student !== 'string') {
+          throw new Error('Students must be a array of strings');
+        }
+      }
+      this._students = students;
+    } else {
+      throw new Error('Students must be a array');
+    }
+  }
 }
