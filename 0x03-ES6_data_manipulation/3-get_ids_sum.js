948c8befb1050d38e5e7262c644511423575846e
sma3in96 <oumouss.ismail5@gmail.com>
2024-06-03 16:07:15 +0100

    task2-4

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,5 @@
+export default function getStudentIdsSum(array) {
+  if (array instanceof Array) {
+    return array.reduce((count, item) => count + item.id, 0);
+  }
+}
---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity(array, city, newGrades) {
+  if (array instanceof Array) {
+    const newarray = array.filter(ele => ele.location === city)
+    return newarray.map((ele) => {
+      return {
+        ...ele,
+        grade: newGrades.filter(grade => grade.studentId === ele.id).map(ele => ele.grade)[0] || 'N/A',
+      };
+    });
+  }
+  return null;
+}
