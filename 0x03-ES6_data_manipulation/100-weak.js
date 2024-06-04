3e939005542d78c674eabaabd6364e59a0265dca
sma3in96 <oumouss.ismail5@gmail.com>
2024-06-04 23:01:04 +0100

    task7

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,14 @@
 export const weakMap = new WeakMap();
 export function queryAPI(object) {
-  const count = weakMap.get(object) || 0;
+  if (object instanceof Object && object.protocol !== undefined && object.name !== undefined) {
+    const count = weakMap.get(object) || 0;
 
-  if (count >= 5) {
-    throw new Error('Endpoint load is high');
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+
+    weakMap.set(object, count + 1);
+  } else {
+    throw new Error('Object is not an API object');
   }
-
-  weakMap.set(object, count + 1);
 }
